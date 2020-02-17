3a7176258a68f2703c61d9b3e29db77fa04d6484
eiglesia <eiglesia@aubay.es>
2020-02-17 17:14:32 +0100

    Agregar archivos de proyecto.

---
+++ HelloWorldClientTcp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldClientTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldServiceRef.HelloWorldServiceClient client = new HelloWorldServiceRef.HelloWorldServiceClient();
+            Console.WriteLine(client.GetMessage("Mike Liu"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HostCmdLineApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace HostCmdLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(MyWCFServices.HelloWorldService),
+                                new Uri("http://localhost:60066/HostCmdLineApp/HelloWorldService.svc"));
+            host.Open();
+            Console.WriteLine("HelloWorldService is now running. ");
+            Console.WriteLine("Press any key to stop it ...");
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
